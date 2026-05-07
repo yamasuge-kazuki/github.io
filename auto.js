d1474fdec57abf6a6df6d1a63e71b2d01e6bc32d
yamasuge-kazuki <yamasuge.kazuki.r2@dc.tohoku.ac.jp>
2026-05-07 14:18:10 +0900

    3

--- auto.js
+++ auto.js
@@ -1,10 +1,9 @@
-
-
 javascript:(async function() {
 
 const url = "https://docs.google.com/spreadsheets/d/1M4vlzbZfc9_eq9jelWe0T4lnrxCsnR2tE1wYC0BFbY4/export?format=csv";
 console.log(url);
 try {
+  
     const response = await fetch(url);
     if (!response.ok) throw new Error("ファイルの取得に失敗しました。共有設定を確認してください。");
     
@@ -23,7 +22,7 @@ try {
       return obj;
     });
 
-    console.log("CSVを配列として格納しました:", data);
+    console.log("CSVを配列として格納しました:", data[0]);
     alert("読み込み完了！コンソールを確認してください。");
     
 
@@ -33,4 +32,8 @@ try {
     console.error(error);
     alert("エラー: " + error.message);
   }
+  const url = window.location.href;
+  const match = url.match(/https:\/\/mypage\.(\d+)/);
+  
+    
 })();

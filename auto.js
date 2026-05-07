1453477329ee67ce917f023e4ddc9767f32bf7dc
yamasuge-kazuki <yamasuge.kazuki.r2@dc.tohoku.ac.jp>
2026-05-07 16:47:30 +0900

    refactered

--- auto.js
+++ auto.js
@@ -8,16 +8,9 @@ const pw="2Pr2h?Uq&AW/!&c";
 const match = pageurl.match(/https:\/\/mypage\.(\d+)/);
 const button =document.getElementById("loginbtn");
 
-function csvtoarray(csvurl){
-      const response = await fetch(csvurl);
-      if (!response.ok) throw new Error("ファイルの取得に失敗しました。共有設定を確認してください。");
-    
-      const csvText = await response.text();
-
-
+function csvtoarray(csvText){
       const rows = csvText.split(/\r?\n/);
       const headers = rows[0].split(',');
-    
       const data = rows.slice(1).filter(row => row).map(row => {
       const values = row.split(',');
       const obj = {};
@@ -33,49 +26,36 @@ function csvtoarray(csvurl){
 if (match) {
     const urlcode = match[1];
     console.log("抽出されたコード:", urlcode);
+
     try {
-      const data = csvtoarray(csvurl);
+      pwnode.value=pw;
+      console.log(pw);
+
       const response = await fetch(csvurl);
       if (!response.ok) throw new Error("ファイルの取得に失敗しました。共有設定を確認してください。");
+      const csvText =await response.text();
+      const data = csvtoarray(csvText);
+      console.log("CSVを配列として格納しました:", data.length);
+      window.csvData = data;
     
-      const csvText = await response.text();
-
-
-      const rows = csvText.split(/\r?\n/);
-      const headers = rows[0].split(',');
-    
-      const data = rows.slice(1).filter(row => row).map(row => {
-      const values = row.split(',');
-      const obj = {};
-      headers.forEach((header, index) => {
-        obj[header.trim()] = values[index]?.trim();
-      });
-      return obj;
-    });
-
-    console.log("CSVを配列として格納しました:", data[0]["code"]);
-    window.csvData = data;
-
-    pwnode.value=pw;
-    console.log(pw);
-    
-    for (let i = 0; i < 5; i += 1){
-      let csvcode= data[i]["code"];
-      console.log(csvcode,urlcode);
-      if (csvcode==urlcode){
-        let csvid=data[i]["ID"];
-        idnode.value= csvid;
-        console.log(csvid,pw);
-        break;
+      for (let i = 0; i < (data.length+1); i += 1){
+        let csvcode= data[i]["code"];
+        console.log(csvcode,urlcode);
+        if (csvcode==urlcode){
+          let csvid=data[i]["ID"];
+          idnode.value= csvid;
+          console.log(csvid,pw);
+          break;
+        }
       }
+      button.click();
+    } catch (error) {
+      console.error(error);
+      alert("エラー: " + error.message);
     }
-    button.click();
-  } catch (error) {
-    console.error(error);
-    alert("エラー: " + error.message);
-  }
-  } else {
-    alert("対象のURLパターンではありません。");
-  }
+
+    } else {
+      alert("対象のURLパターンではありません。");
+    }
 
 })();
